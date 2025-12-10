508252811426fc0a7ea2e1800b3c22fd841be111
Poonam <nityag606@gmail.com>
2025-12-10 19:04:12 +0530

    FINDING SQUARE ROOT OF A NUMBER

---
+++ BINARY SEARCH/BS IN ANSWERS/FINDING SQUARE ROOT OF A NUMBER.cpp
@@ -0,0 +1,23 @@
+//tc=o(log n)
+
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        // code here
+        int low=1;
+        int high=n;
+        int ans=1;
+        while(low<=high){
+            long long mid=high-(high-low)/2;
+            long long val=mid*mid;
+            if(val<=n){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
