8f73e895276a12d4a398e6d958187555150aad9a
Poonam <nityag606@gmail.com>
2025-12-30 20:47:22 +0530

    PAINTER'S PARTITION PROBLEM

---
+++ BINARY SEARCH/BS IN ANSWERS/PAINTER'S PARTION PROBLEM.cpp
@@ -0,0 +1,54 @@
+//to minimize the maximum time taken by a painter to paint all boards
+//tc: O(nlogm) , m is the sum of all board lengths , n is number of boards
+//sc: O(1)
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+        int maxN(vector<int>& arr){
+        int maxi=INT_MIN;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]>maxi){
+                maxi=arr[i];
+            }
+        }
+        return maxi;
+    }
+    int summ(vector<int>& arr){
+        int sum=0;
+        for(int i=0;i<arr.size();i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+    int alct(vector<int>& arr, int pg,int k){
+        int st=1;
+        int pages=0;
+        for(int i=0;i<arr.size();i++){
+            if(pages+arr[i]<=pg){
+                pages+=arr[i];
+            }
+            else{
+                st+=1;
+                pages=arr[i];
+            }
+        }
+        return st;
+    }
+    int findPages(vector<int> &arr, int k) {
+        if(k>arr.size())return -1;
+        int low=maxN(arr),high=summ(arr);
+        while(low<=high){
+            int mid=(low+high)/2;
+            int ans=alct(arr,mid,k);
+            if(ans>k)low=mid+1;
+            else high=mid-1;
+            
+        }
+        return low;
+    }
+    int minTime(vector<int>& arr, int k) {
+        // code here
+        return findPages(arr,k);
+    }
+};
