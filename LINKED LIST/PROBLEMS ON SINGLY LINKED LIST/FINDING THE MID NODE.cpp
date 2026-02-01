6949e60398b511b9ace4808639a5b88a5a58dac2
Poonam <nityag606@gmail.com>
2026-02-01 12:47:44 +0530

    lc-876

---
+++ LINKED LIST/PROBLEMS ON SINGLY LINKED LIST/FINDING THE MID NODE.cpp
@@ -0,0 +1,26 @@
+//LC-876. Middle of the Linked List
+//TC: O(N)
+//SC: O(1)
+//Definition for singly-linked list.
+#include<bits/stdc++.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+
+    }
+};
