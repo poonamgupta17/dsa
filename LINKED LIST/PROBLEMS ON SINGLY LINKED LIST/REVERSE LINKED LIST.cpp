fd64403527e152ce31080a9b58437e20c4e6363a
Poonam <nityag606@gmail.com>
2026-02-01 15:09:45 +0530

    REVERSE LINKED LIST( ITERATIVE )

---
+++ LINKED LIST/PROBLEMS ON SINGLY LINKED LIST/REVERSE LINKED LIST.cpp
@@ -0,0 +1,29 @@
+//LC-206. Reverse Linked List
+//TC: O(N)
+//SC: O(1)
+#include<bits/stdc++.h>
+using namespace std;
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+//iterative approach
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev=NULL;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            ListNode* front=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;
+        }
+        return prev;
+    }
+};
