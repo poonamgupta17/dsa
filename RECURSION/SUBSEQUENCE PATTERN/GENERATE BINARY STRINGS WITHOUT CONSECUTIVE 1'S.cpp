9ea1441573b45cd65d83c5d19a60594767ff35a2
Poonam <nityag606@gmail.com>
2026-02-24 23:33:21 +0530

    CONSECUTIVE 1'S NOT ALLOWED

---
+++ RECURSION/SUBSEQUENCE PATTERN/GENERATE BINARY STRINGS WITHOUT CONSECUTIVE 1'S.cpp
@@ -0,0 +1,23 @@
+//to return the count of binary strings of length n without consecutive 1's
+#include <bits/stdc++.h>
+using namespace std;
+// User function template for C++
+class Solution {
+  public:
+    // #define ll long long
+    long long countStrings(int n) { 
+        // code here
+        int oneend=1;
+        int zeroend=1;
+        int sum=oneend+zeroend;
+        if(n==1)return sum;
+        int i=2;
+        while(i<=n){
+            oneend=zeroend ;
+            zeroend=sum ;
+            sum=zeroend+oneend % (1000000007); //to avoid overflow we will take mod with 10^9+7
+            i++;
+        }
+        return sum;
+    }
+};
