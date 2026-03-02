4a00e1516c4865195b2aed806cad79f8177ebef0
Poonam <nityag606@gmail.com>
2026-03-03 01:59:55 +0530

    COUNT SUBSEQUENCES WITH SUM K

--- RECURSION/SUBSEQUENCE PATTERN/COUNT SUBSEQUENCES WITH SUM K.cpp
+++ RECURSION/SUBSEQUENCE PATTERN/COUNT SUBSEQUENCES WITH SUM K.cpp
@@ -4,7 +4,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int printS(int ind,int sum,int k,vector<int>& arr,int n){
-        
+        if(sum>k)return 0;//valid only if we have +ve numbers in array.
         if(ind==n){
             if(sum==k)return 1;
             else return 0;
