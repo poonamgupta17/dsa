5ee7db307ce0f2326fe6028f2085378099b0371b
Poonam <nityag606@gmail.com>
2026-04-07 16:50:47 +0530

    lc - 90

--- RECURSION/SUBSEQUENCE PATTERN/SUBSETS 2.cpp
+++ RECURSION/SUBSEQUENCE PATTERN/SUBSETS 2.cpp
@@ -1,4 +1,4 @@
-//lc-90 to find all the subsets of a given set of numbers, which may contain duplicates, and return the solution in sorted order. The solution set must not contain duplicate subsets. 
+//lc-90  to find all the subsets of a given set of numbers, which may contain duplicates, and return the solution in sorted order. The solution set must not contain duplicate subsets. 
 //tc=O(2^n * n)
 //sc=O(2^n * k) where k is the average length of the subsets because we are storing all the subsets in the ans vector and in the worst case we can have all the elements as 1 and we can use them n times to get the target
 #include <bits/stdc++.h>
