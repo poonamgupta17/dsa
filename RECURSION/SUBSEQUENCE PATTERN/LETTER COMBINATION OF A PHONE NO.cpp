c671eb91874c226891c0ed3b4103a133f30a13ae
Poonam <nityag606@gmail.com>
2026-05-26 13:10:45 +0530

    lc-17

---
+++ RECURSION/SUBSEQUENCE PATTERN/LETTER COMBINATION OF A PHONE NO.cpp
@@ -0,0 +1,35 @@
+//lc 17 letter combinations of a phone number
+//time complexity O(n*4^n) where n is the length of the input string
+//space complexity O(n* 4^n)+ O(n) where n is the length of the input string
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void help(int i,string & s,string &temp,vector<string>&ans,unordered_map<char,string>&m){
+        if(i==s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        string str=m[s[i]];
+        for(int j=0;j<str.size();j++){
+            temp.push_back(str[j]);
+            help(i+1,s,temp,ans,m);
+            temp.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        unordered_map<char,string>m;
+        m['2']="abc";
+        m['3']="def";
+        m['4']="ghi";
+        m['5']="jkl";
+        m['6']="mno";
+        m['7']="pqrs";
+        m['8']="tuv";
+        m['9']="wxyz";
+        vector<string>ans;
+        string temp;
+        help(0 ,digits,temp,ans,m);
+        return ans;
+    }
+};
