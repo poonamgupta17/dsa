9aa6c368da1029b00c4643d3189b56c271fe43ba
Poonam <nityag606@gmail.com>
2026-05-28 00:28:43 +0530

    lc-70

--- DYNAMIC PROGRAMMING/CLIMBING STAIRS.cpp
+++ DYNAMIC PROGRAMMING/CLIMBING STAIRS.cpp
@@ -35,3 +35,21 @@ public:
         return dp[n];
     }
 };
+//space optimized approach
+//tc: O(n)
+//sc: O(1)
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2)return n;
+        int a=1;
+        int b=2;
+        int c;
+        for(int i=3;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+};
