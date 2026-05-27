14c0742a43e4978859afa27689043d0644d37bcb
Poonam <nityag606@gmail.com>
2026-05-27 22:28:57 +0530

    lc-509

--- DYNAMIC PROGRAMMING/FIBONACCI.cpp
+++ DYNAMIC PROGRAMMING/FIBONACCI.cpp
@@ -34,3 +34,21 @@ public:
         return dp[n];
     }
 };
+//space optimized approach
+//tc: O(n)
+//sc: O(1)
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1)return n;
+        int a=0;
+        int b=1;
+        int c;
+        for(int i=1;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+};
