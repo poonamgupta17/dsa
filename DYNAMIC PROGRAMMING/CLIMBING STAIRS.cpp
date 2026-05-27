686f5826891da8ba55d828ab007999768d44f9f6
Poonam <nityag606@gmail.com>
2026-05-28 00:28:54 +0530

    lc-70

--- DYNAMIC PROGRAMMING/CLIMBING STAIRS.cpp
+++ DYNAMIC PROGRAMMING/CLIMBING STAIRS.cpp
@@ -1,10 +1,22 @@
 //lc-70 climbing stairs
 
-// recursive approach with memoization
+//recursive approach
 //tc: O(2^n)
-//sc: O(n) for dp array and O(n) for recursive stack space
+//sc: O(n) for recursive stack space
 #include<bits/stdc++.h>
 using namespace std;
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<0)return 0;
+        if(n==0)return 1;
+        return climbStairs(n-1)+climbStairs(n-2);
+    }
+};
+
+// recursive approach with memoization
+//tc: O(n)
+//sc: O(n) for dp array and O(n) for recursive stack space
 class Solution {
 public:
     int solve(int n,vector<int>&dp){
@@ -27,9 +39,10 @@ class Solution {
 public: 
     int climbStairs(int n) {
         vector<int>dp(46,-1);
-        dp[0]=1;
+        dp[0]=0;
         dp[1]=1;
-        for(int i=2;i<=n;i++){
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
