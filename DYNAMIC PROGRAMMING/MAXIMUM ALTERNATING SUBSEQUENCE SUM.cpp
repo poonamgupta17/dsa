f4ad3062cf94ccea816b57fc2ea9b183f92a181b
Poonam <nityag606@gmail.com>
2026-06-03 01:39:38 +0530

    lc-1911

---
+++ DYNAMIC PROGRAMMING/MAXIMUM ALTERNATING SUBSEQUENCE SUM.cpp
@@ -0,0 +1,29 @@
+//lc-1911 
+//find the maximum alternating subsequence sum(sum of elements at even indices - sum of elements at odd indices) in the given array
+
+//recursive + memoization
+//tc: O(n)
+//sc: O(n) for recursion stack + O(n) for dp array
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    typedef long long ll;
+    long long t[100001][2];
+    ll solve(int i,int n,vector<int>& nums,bool flag){
+        if(i>=n)return 0;
+        if(t[i][flag]!=-1)return t[i][flag];
+        ll skip=solve(i+1,n,nums,flag);
+        ll val=nums[i];
+        if(flag==false){
+            val=-val;
+        }
+        ll take=val+solve(i+1,n,nums,!flag);
+        return t[i][flag]=max(take,skip);
+    }
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n=nums.size();
+        memset(t,-1,sizeof(t));
+        return solve(0,n,nums,true);
+    }
+};
