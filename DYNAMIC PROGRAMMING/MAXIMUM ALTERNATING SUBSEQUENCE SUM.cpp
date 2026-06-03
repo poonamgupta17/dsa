1a0848036a01a9f25f1ea9361e1cf8fe844df899
Poonam <nityag606@gmail.com>
2026-06-04 02:22:15 +0530

    lc-1911

--- DYNAMIC PROGRAMMING/MAXIMUM ALTERNATING SUBSEQUENCE SUM.cpp
+++ DYNAMIC PROGRAMMING/MAXIMUM ALTERNATING SUBSEQUENCE SUM.cpp
@@ -27,3 +27,18 @@ public:
         return solve(0,n,nums,true);
     }
 };
+//bottom up
+//tc: O(n)
+//sc: O(n) for dp array
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<long long>>t(n+1,vector<long long>(2,0));
+        for(int i=1;i<n+1;i++){
+            t[i][0]=max(t[i-1][1]-nums[i-1],t[i-1][0]);
+            t[i][1]=max(t[i-1][0]+nums[i-1],t[i-1][1]);
+        }
+        return max(t[n][0],t[n][1]);
+    }
+};
