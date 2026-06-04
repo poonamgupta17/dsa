b6dfefaa41df7a8fb88685dca647c8ab8c1b3ac9
Poonam <nityag606@gmail.com>
2026-06-05 02:10:21 +0530

    lc-300

--- DYNAMIC PROGRAMMING/LIS(longest increasing subsequence).cpp
+++ DYNAMIC PROGRAMMING/LIS(longest increasing subsequence).cpp
@@ -27,3 +27,23 @@ public:
         return solve(0,-1,n,nums);
     }
 };
+//bottom up
+//tc: O(n^2)
+//sc: O(n) for dp array
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>t(n,1);
+        int maxAns=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    t[i]=max(t[i],t[j]+1);
+                    maxAns=max(t[i],maxAns);    
+                }
+            }
+        }
+        return maxAns;
+    }
+};
