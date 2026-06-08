21c0ae8d7d3ec5c0922cad68b33b7aa4f4e3aca5
Poonam <nityag606@gmail.com>
2026-06-08 23:47:38 +0530

    lc-2926

--- DYNAMIC PROGRAMMING/LIS/MAXIMUM BALANCED SUBSEQUENCE SUM.cpp
+++ DYNAMIC PROGRAMMING/LIS/MAXIMUM BALANCED SUBSEQUENCE SUM.cpp
@@ -69,3 +69,30 @@ public:
         return maxSum > maxEl ? maxSum : maxEl;
     }
 };
+
+//optimized approach using maps
+//tc= O(nlogn) in worst case (when all elements are distinct)
+//sc= O(n) in worst case
+class Solution {
+public:
+    long long maxBalancedSubsequenceSum(vector<int>& nums) {
+        int n=nums.size();
+        long long result=INT_MIN;
+        map<int,long long>mp; //nums[i]-i
+        for(int i=0;i<n;i++){
+            auto it=mp.upper_bound(nums[i]-i); //it->first=nums[j]-j, it->second=sum
+            long long sum=nums[i];
+            if(it!=mp.begin()){
+                it--;
+                sum+=it->second;
+            }
+            mp[nums[i]-i]=max(mp[nums[i]-i],sum);
+            it=mp.upper_bound(nums[i]-i);
+            while(it!=mp.end()&& it->second<=sum){
+                mp.erase(it++);
+            }
+        result=max(result,sum);
+        }
+        return result;
+    }
+};
