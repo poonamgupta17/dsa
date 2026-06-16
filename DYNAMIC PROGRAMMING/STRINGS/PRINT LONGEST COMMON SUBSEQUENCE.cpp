c99596344f483985daf3bd09b570b1e4fabeb212
Poonam <nityag606@gmail.com>
2026-06-16 14:12:16 +0530

    lc-1143(print lcs)

--- DYNAMIC PROGRAMMING/STRINGS/PRINT LONGEST COMMON SUBSEQUENCE.cpp
+++ DYNAMIC PROGRAMMING/STRINGS/PRINT LONGEST COMMON SUBSEQUENCE.cpp
@@ -1,5 +1,6 @@
 //lc-1143
 
+//bottom up(forward indexing)
 //tc: O(n*m)
 //sc: O(n*m) for dp array
 #include <bits/stdc++.h>
@@ -43,5 +44,49 @@ public:
         cout<<lcs<<endl;
         return t[n][m];
         
+    }
+};
+//bottom up(reverse indexing)
+class Solution {
+public:
+    int longestCommonSubsequence(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        // Fill from bottom-right to top-left
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+
+                if (s1[i] == s2[j]) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+
+        // Reconstruct the LCS string
+        string lcs = "";
+        int i = 0, j = 0;
+
+        while (i < n && j < m) {
+            if (s1[i] == s2[j]) {
+                lcs.push_back(s1[i]);
+                i++;
+                j++;
+            } else {
+                if (dp[i + 1][j] >= dp[i][j + 1]) {
+                    i++;
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        cout << lcs << endl;
+
+        return dp[0][0];
     }
 };
