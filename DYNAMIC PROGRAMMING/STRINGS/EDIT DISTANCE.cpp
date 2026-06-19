f14279575a7b6c269f8a2cc29018b82b49160ad6
Poonam <nityag606@gmail.com>
2026-06-19 23:54:31 +0530

    lc-72

--- DYNAMIC PROGRAMMING/STRINGS/EDIT DISTANCE.cpp
+++ DYNAMIC PROGRAMMING/STRINGS/EDIT DISTANCE.cpp
@@ -14,17 +14,19 @@ public:
         if(i==n){
             return m-j;
         }
-        if(j==m){
+        else if(j==m){
             return n-i;
         }
         if(t[i][j]!=-1)return t[i][j];
         if(s1[i]==s2[j]){
             return solve(i+1,j+1,s1,s2);
         }
-        int insert=1+solve(i,j+1,s1,s2);
-        int remove=1+solve(i+1,j,s1,s2);
-        int replace=1+solve(i+1,j+1,s1,s2);
-        return t[i][j]= min({insert,remove,replace});
+        else{
+            int insert=1+solve(i,j+1,s1,s2);
+            int remove=1+solve(i+1,j,s1,s2);
+            int replace=1+solve(i+1,j+1,s1,s2);
+            return t[i][j]= min({insert,remove,replace});
+        }
     }
     int minDistance(string s1, string s2) {
         n=s1.length();
