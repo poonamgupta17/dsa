78b1adb756fafe5850dfc70eefe142c8082c00d4
Poonam <nityag606@gmail.com>
2026-06-20 02:02:09 +0530

    lc-72

--- DYNAMIC PROGRAMMING/STRINGS/EDIT DISTANCE.cpp
+++ DYNAMIC PROGRAMMING/STRINGS/EDIT DISTANCE.cpp
@@ -65,31 +65,14 @@ public:
         return solve(n,m,s1,s2);
     }
 };
-//tabulation
+//bottom up approach (table filling)
 //tc: O(n*m)
 //sc: O(n*m)
 class Solution {
 public:
-    int n,m;
-    int t[501][501];
-    int solve(int m,int n, string s1,string s2){
-        if(m==0 || n==0){
-            return m+n;
-        }
-        if(t[m][n]!=-1)return t[m][n];
-        if(s1[m-1]==s2[n-1]){
-            return solve(m-1,n-1,s1,s2);
-        }
-        else{
-            int insert=1+solve(m,n-1,s1,s2);
-            int remove=1+solve(m-1,n,s1,s2);
-            int replace=1+solve(m-1,n-1,s1,s2);
-            return t[m][n]= min({insert,remove,replace});
-        }
-    }
     int minDistance(string s1, string s2) {
-        m=s1.length();
-        n=s2.length();
+        int m=s1.length();
+        int n=s2.length();
         vector<vector<int>>t(m+1,vector<int>(n+1));
         for(int i=0;i<m+1;i++){
             for(int j=0;j<n+1;j++){
