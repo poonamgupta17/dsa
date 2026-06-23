c96a047ea0a112cb9c88303434df02ea2051910f
Poonam <nityag606@gmail.com>
2026-06-23 22:47:52 +0530

    lc-647

--- DYNAMIC PROGRAMMING/STRINGS/PALINDROME SUBSTRING.cpp
+++ DYNAMIC PROGRAMMING/STRINGS/PALINDROME SUBSTRING.cpp
@@ -64,3 +64,57 @@ public:
         return count;
     }
 };
+//bottom up dp approach
+//tc=O(n^2)
+//sc=O(n^2)
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n=s.length();
+        vector<vector<bool>>t(n,vector<bool>(n,false));
+        int count=0;
+        for(int l=1;l<=n;l++){
+            for(int i=0;l+i-1<n;i++){
+                int j=l+i-1;
+                if(i==j){
+                    t[i][j]=true;
+                }
+                else if(i+1==j){
+                    t[i][j]=(s[i]==s[j]);   
+                }
+                else{
+                    t[i][j]=(s[i]==s[j] && t[i+1][j-1]);
+                }
+                if(t[i][j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+//expanding from mid points approach
+//tc=O(n^2)
+//sc=O(1)
+class Solution {
+public:
+    int count;
+    int check(int i,int j,string &s,int n){
+        while((i>=0 && j<n)&&s[i]==s[j]){
+            count++;
+            i--;
+            j++;
+        }
+        return count;
+    }
+    int countSubstrings(string s) {
+        int n=s.length();
+        int a,b;
+        count = 0;
+        for(int i=0;i<n;i++){
+            check(i,i,s,n);
+            check(i,i+1,s,n);
+        }
+        return count;
+    }
+};
