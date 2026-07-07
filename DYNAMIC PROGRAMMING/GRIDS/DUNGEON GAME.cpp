22b779dcefcb17deb8747b2585bb39116590c2ee
Poonam <nityag606@gmail.com>
2026-07-08 02:29:42 +0530

    lc-174

--- DYNAMIC PROGRAMMING/GRIDS/DUNGEON GAME.cpp
+++ DYNAMIC PROGRAMMING/GRIDS/DUNGEON GAME.cpp
@@ -68,3 +68,29 @@ public:
         return solve(0,0,dungeon);
     }
 };
+//bottom up dp
+//tc= O(m*n) 
+//sc= O(m*n)
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int m=dungeon.size();
+        int n=dungeon[0].size(); 
+        vector<vector<int>>t(m,vector<int>(n));
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1 && j==n-1){
+                    if(dungeon[i][j]>0)t[i][j]= 1;
+                    else t[i][j]= abs(dungeon[i][j])+1;
+                }
+                else{
+                    int down=i+1<m ? t[i+1][j]: INT_MAX ;
+                    int right=j+1<n ? t[i][j+1]: INT_MAX;
+                    int res=min(down,right)-dungeon[i][j];
+                    t[i][j]= res<=0 ? 1:res;
+                }  
+            }
+        }
+        return t[0][0]; //minimum health required to reach the princess(m-1,n-1) from the starting cell (0,0)
+    }
+};
