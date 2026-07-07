f4abc7c58929c9ba04c6a5b80526446bbd794b64
Poonam <nityag606@gmail.com>
2026-07-08 01:31:51 +0530

    lc-1594

---
+++ DYNAMIC PROGRAMMING/GRIDS/DUNGEON GAME.cpp
@@ -0,0 +1,3 @@
+//lc-174
+
+//return the minimum initial health required to reach the princess in a m*n grid (allowing only right and down moves), where each cell has a health value (negative for damage, positive for health gain)
