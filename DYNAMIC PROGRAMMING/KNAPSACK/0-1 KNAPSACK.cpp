2aa6bcc430311cfe3d75231e8fc5a4a5d0b1e268
Poonam Gupta <nityag606@gmail.com>
2026-07-21 20:02:29 +0530

    0-1 KNAPSACK

--- DYNAMIC PROGRAMMING/KNAPSACK/0-1 KNAPSACK.cpp
+++ DYNAMIC PROGRAMMING/KNAPSACK/0-1 KNAPSACK.cpp
@@ -88,3 +88,30 @@ class Solution {
         return t[n][W];
     }
 };
+//space optimized bottom up approach
+//tc= O(n*W)
+//sc= O(W)
+class Solution {
+  public:
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        // code here
+        int n=val.size();
+        vector<int>curr(W+1,0);
+        vector<int>prev(W+1,0);
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<W+1;j++){
+                int take=0;
+                int skip=0;
+                if(wt[i-1]<=j){
+                    take=val[i-1]+prev[j-wt[i-1]]; //t[i-1]=prev, 
+                }
+                skip=prev[j];
+                curr[j]=max(take,skip);
+            }
+            //move current row->prev row
+            prev=curr;
+        }
+        return prev[W];
+    }
+};
